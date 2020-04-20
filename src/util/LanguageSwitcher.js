40ccb1ea2405d074223fc59d887937933efc5abf
omoya_fbit <omoya@fundaciobit.org>
2020-04-21 01:01:05 +0200

    Changed class to className tag properties

--- src/util/LanguageSwitcher.js
+++ src/util/LanguageSwitcher.js
@@ -7,8 +7,8 @@ import './LanguageSwitcher.css'
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation()
   return (
-    <div class="langSelectors">
-      <span class="langSpan"
+    <div className="langSelectors">
+      <span className="langSpan"
         key="es"
         onClick={() => {
           i18n.changeLanguage('es')
@@ -17,7 +17,7 @@ const LanguageSwitcher = () => {
         >
         ES
       </span>
-      <span class="langSpan"
+      <span className="langSpan"
         key="ca"
         onClick={() => {
           i18n.changeLanguage('ca')
