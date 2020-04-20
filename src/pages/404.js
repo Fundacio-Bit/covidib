1fa200d27102365c30683bc0f600f84a464a98c3
Óscar Moya Mesa <omoya@fundaciobit.org>
2020-04-20 14:08:50 +0200

    Added 404 page, styles and image (#24)

---
+++ src/pages/404.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from 'gatsby'
+import { Styled } from 'theme-ui'
+import errorImage from '../static/404Error.svg'
+
+import './index.css'
+
+export default () => (
+  <Styled.root>
+    <main class="errorContainer" >
+      <div class="errorDiv">
+        <div>  
+          <img src={errorImage} alt="Error 404" height="200" width="200" />
+        </div>
+        <div >  
+          <Styled.h1>No s'ha trobat aquesta pàgina</Styled.h1>
+        </div>
+        <div >
+          <p>Pot tornar a la pàgina d'inici seguint l'enllaç:</p>
+        </div>
+        <div >
+          <Link to="/">Tornar a Inici</Link>
+        </div>
+      </div>
+
+    </main>
+  </Styled.root>
+)
