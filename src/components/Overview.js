9fa8ef57a64bf01b576afe8c1efca2142693562c
Álvaro Medina Ballester <amedina@fundaciobit.org>
2020-04-21 18:32:20 +0200

    Fixed names

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,56 +1,83 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { useTranslation } from "react-i18next";
+import last from "lodash/last";
 
-const Overview = ({ currentNode }) => {
-  const { t } = useTranslation()
+const Overview = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allDataJson(sort: { fields: data, order: DESC }, limit: 1) {
+        nodes {
+          nous_positius
+          percentatge_increment
+          positius_actius
+          positius_acumulats
+          professionals_positius
+          proves_laboratori
+          uci
+          uvac_actius
+          curats
+          exitus
+          hospitalitzats
+        }
+      }
+    }
+  `);
+  const { t } = useTranslation();
+  const currentNode = last(data.allDataJson.nodes);
   return (
     <section id="overview">
-      <h2>{t('situ_actual')}</h2>
+      <h2>{t("situ_actual")}</h2>
       <ul>
         <li>
           {currentNode.nous_positius}
-          <p>{t('nous_positius')}</p>
+          <p>{t("nous_positius")}</p>
         </li>
         <li>
-          {currentNode.percentatge_increment}%<p>{t('percentatge_increment')}</p>
+          {currentNode.percentatge_increment}%
+          <p>{t("percentatge_increment")}</p>
         </li>
         <li>
           {currentNode.curats}
-          <p>{t('curats')}</p>
+          <p>{t("total_curats")}</p>
         </li>
         <li>
           {currentNode.exitus}
-          <p>{t('exitus')}</p>
+          <p>{t("total_exitus")}</p>
         </li>
         <li>
           {currentNode.positius_actius}
-          <p>{t('positius_actius')}</p>
+          <p>{t("positius_actius")}</p>
         </li>
       </ul>
       <ul>
         <li>
           {currentNode.positius_acumulats}
-          <p>{t('positius_acumulats')}</p>
+          <p>{t("positius_acumulats")}</p>
         </li>
         <li>
           {currentNode.hospitalitzats}
-          <p>{t('hospitalitzats')}</p>
+          <p>{t("hospitalitzats_avui")}</p>
         </li>
         <li>
           {currentNode.uci}
-          <p>UCI</p>
+          <p>{t("uci_avui")}</p>
         </li>
         <li>
           {currentNode.professionals_positius}
-          <p>{t('professionals_positius')}</p>
+          <p>{t("professionals_positius")}</p>
         </li>
         <li>
           {currentNode.proves_laboratori}
-          <p>{t('proves_laboratori')}</p>
+          <p>{t("proves_laboratori")}</p>
+        </li>
+        <li>
+          {currentNode.uvac_actius}
+          <p>{t("uvac_actius")}</p>
         </li>
       </ul>
     </section>
-  )
-}
+  );
+};
 
-export default Overview
+export default Overview;
