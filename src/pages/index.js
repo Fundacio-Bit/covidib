dc3896f9f3d15682c49fb21c226c7fb5099aa661
omoya_fbit <omoya@fundaciobit.org>
2020-04-21 09:16:23 +0200

    Deaths per capita moving average series (prettified)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import { useStaticQuery, graphql } from 'gatsby'
-import { Styled } from 'theme-ui'
-import 'moment/locale/ca'
-import 'moment/locale/es'
-import last from 'lodash/last'
-import '../util/i18n'
-import { useTranslation } from 'react-i18next'
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import { Styled } from "theme-ui";
+import "moment/locale/ca";
+import "moment/locale/es";
+import last from "lodash/last";
+import "../util/i18n";
+import { useTranslation } from "react-i18next";
 
-import LogDeaths from '../charts/LogDeaths'
-import LogCases from '../charts/LogCases'
-import StackedCases from '../charts/StackedCases'
+import LogDeaths from "../charts/LogDeaths";
+import LogCases from "../charts/LogCases";
+import StackedCases from "../charts/StackedCases";
 
-import StatChart from '../components/StatChart'
-import StatChartComparison from '../components/StatChartComparison'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import Overview from '../components/Overview'
-import IslandsOverview from '../components/IslandsOverview'
-import Colors from '../constants/Colors'
+import StatChart from "../components/StatChart";
+import StatChartComparison from "../components/StatChartComparison";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Overview from "../components/Overview";
+import IslandsOverview from "../components/IslandsOverview";
+import Colors from "../constants/Colors";
 
-import './index.css'
+import "./index.css";
 
 const Index = () => {
   const data = useStaticQuery(graphql`
@@ -55,42 +55,53 @@ const Index = () => {
         }
       }
     }
-  `)
+  `);
 
-  let nodes = data.allDataJson.nodes.sort((a, b) => a.timestamp - b.timestamp)
+  let nodes = data.allDataJson.nodes.sort((a, b) => a.timestamp - b.timestamp);
+
+  function sayHi(name) {
+    return "Result: " + name;
+  }
 
   // 1.188 million inhabitants
   // to calculate deaths per million inhabitants we apply a 1/1.188 factor
-  let deathsPerMillion = nodes.map((item) => { return item.exitus/1.188 })
+  let deathsPerMillion = nodes.map((item) => {
+    return item.exitus / 1.188;
+  });
 
   const calculateMovingAverageSeries = (numbersArray, range) => {
-    let resultingSeries = []
-    for (let i = 0; i < (numbersArray.length + 1 - range); i++) {
-      let dataInRange = numbersArray.slice(i, i+range)
-      let sum = dataInRange.reduce((previous, current) => current += previous);
-      let avg = (sum / dataInRange.length).toFixed(2)
-      resultingSeries.push(avg)    
+    let resultingSeries = [];
+    for (let i = 0; i < numbersArray.length + 1 - range; i++) {
+      let dataInRange = numbersArray.slice(i, i + range);
+      let sum = dataInRange.reduce(
+        (previous, current) => (current += previous)
+      );
+      let avg = (sum / dataInRange.length).toFixed(2);
+      resultingSeries.push(avg);
     }
-    return resultingSeries
-  }
-  let lastWeekNodes = nodes.slice(Math.max(nodes.length - 7, 0))
+    return resultingSeries;
+  };
+  let lastWeekNodes = nodes.slice(Math.max(nodes.length - 7, 0));
   // 1.188 million inhabitants
   // to calculate deaths per million inhabitants we apply a 1/1.188 factor
-  let lastWeekDeathsPerMillion = lastWeekNodes.map((item) => { return item.exitus/1.188 })
+  let lastWeekDeathsPerMillion = lastWeekNodes.map((item) => {
+    return item.exitus / 1.188;
+  });
   // moving average last day
-  let sum = lastWeekDeathsPerMillion.reduce((previous, current) => current += previous);
-  let avg = (sum / lastWeekDeathsPerMillion.length).toFixed(2)
-  console.log(avg)
+  let sum = lastWeekDeathsPerMillion.reduce(
+    (previous, current) => (current += previous)
+  );
+  let avg = (sum / lastWeekDeathsPerMillion.length).toFixed(2);
+  console.log(avg);
 
   // moving average series
-  let movingAverageSeries = calculateMovingAverageSeries(deathsPerMillion, 7)
-  console.log(movingAverageSeries)
-  
+  let movingAverageSeries = calculateMovingAverageSeries(deathsPerMillion, 7);
+  console.log(movingAverageSeries);
 
-  const currentNode = last(nodes)
-  let prevDayNode = nodes[nodes.length - 2]
+  const currentNode = last(nodes);
+  let prevDayNode = nodes[nodes.length - 2];
 
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <Styled.root>
@@ -99,14 +110,14 @@ const Index = () => {
         <div className="container">
           <Overview currentNode={currentNode} prevDayNode={prevDayNode} />
           <StatChart
-            title={t('curats_title')}
+            title={t("curats_title")}
             data={nodes}
             yKey="curats"
             xKey="data"
             color={Colors.green}
           />
           <StatChartComparison
-            title={t('actius_curats_title')}
+            title={t("actius_curats_title")}
             data={nodes}
             yKey="positius_actius"
             y2Key="curats"
@@ -114,9 +125,9 @@ const Index = () => {
             color={Colors.purple}
             color2={Colors.green}
           />
-          <StackedCases title={t('actius_curats_title')} />
+          <StackedCases title={t("actius_curats_title")} />
           <IslandsOverview
-            islands={t('mallorca')}
+            islands={t("mallorca")}
             hospitalized={currentNode.mallorca_hospitalizats}
             uci={currentNode.mallorca_uci}
             positiveProfs={currentNode.mallorca_professionals_positius}
@@ -124,7 +135,7 @@ const Index = () => {
             uvac={currentNode.mallorca_uvac}
           />
           <IslandsOverview
-            islands={t('menorca')}
+            islands={t("menorca")}
             hospitalized={currentNode.menorca_hospitalizats}
             uci={currentNode.menorca_uci}
             positiveProfs={currentNode.menorca_professionals_positius}
@@ -132,7 +143,7 @@ const Index = () => {
             uvac={currentNode.menorca_uvac}
           />
           <IslandsOverview
-            islands={t('pitiuses')}
+            islands={t("pitiuses")}
             hospitalized={currentNode.ibiza_hospitalizats}
             uci={currentNode.ibiza_uci}
             positiveProfs={currentNode.ibiza_professionals_positius}
@@ -142,7 +153,7 @@ const Index = () => {
           <LogDeaths />
           <LogCases />
           <StatChart
-            title={t('casos_nous_title')}
+            title={t("casos_nous_title")}
             data={nodes}
             yKey="nous_positius"
             xKey="data"
@@ -150,7 +161,7 @@ const Index = () => {
           />
           <StatChart
             isPercentageChart
-            title={t('percentatge_increment_title')}
+            title={t("percentatge_increment_title")}
             data={nodes}
             yKey="percentatge_increment"
             xKey="data"
@@ -160,7 +171,7 @@ const Index = () => {
         </div>
       </main>
     </Styled.root>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
