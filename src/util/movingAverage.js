89ac3f80a9fa3d27a81c5ce33bd6694e4d34bcdc
omoya_fbit <omoya@fundaciobit.org>
2020-04-24 10:13:49 +0200

    Infer dailyCounts from cumulativeCounts

--- src/util/movingAverage.js
+++ src/util/movingAverage.js
@@ -1,27 +1,55 @@
 import last from "lodash/last";
 
 /**
+  * @desc infers a series of daily deaths count from a cumulative series of deaths.
+  * @param Array $dailyCumulativeCountsArray - an array of objects that represents a
+  * deaths time series.
+  * @return Array - a series of daily deaths count .
+*/
+const calculateDailyDeathsCountSeries = (dailyCumulativeCountsArray) => {
+  //first cumulative array element should be taken as is
+  let dailyCountsArraySeries = [
+    {
+      deaths_per_date: dailyCumulativeCountsArray[0].exitus,
+      data: dailyCumulativeCountsArray[0].data}
+  ]
+  //first cumulative array element should be processed
+  for (let i = 1; i < dailyCumulativeCountsArray.length; i++) {
+    dailyCountsArraySeries.push(
+      {
+        deaths_per_date: (dailyCumulativeCountsArray[i].exitus - dailyCumulativeCountsArray[i-1].exitus),
+        data: dailyCumulativeCountsArray[i].data
+      }
+    );
+  }
+  return dailyCountsArraySeries
+}
+
+/**
   * @desc calculates a moving average series for a given time window.
-  * @param Array $dailyCountsArray - an array of objects {deaths_per_million, date} that represents a daily-based time series.
+  * @param Array $dailyCountsArray - an array of objects that represents a
+  * deaths time series.
   * @param window $int - the number of days that should be considered to calculate the moving average.
   * @return Array - a series of moving averages per day.
 */
 
-const calculateMovingAverageSeries = (dailyCountsArray, window) => {
+const calculateMovingAverageSeries = (nodes, window) => {
   let resultingSeries = [];
-  for (let i = 0; i < dailyCountsArray.length + 1 - window; i++) {
-    let dataInWindow = dailyCountsArray.slice(i, i + window);
-    console.log(dataInWindow)
+  let dailyCountsArraySeries = calculateDailyDeathsCountSeries(nodes)
+  for (let i = 0; i < dailyCountsArraySeries.length + 1 - window; i++) {
+    let dataInWindow = dailyCountsArraySeries.slice(i, i + window);
     let lastDateInWindow = last(dataInWindow).data
-    let deaths_per_million = dataInWindow.map((item) => {
-      return item.deaths_per_million; });
-    let sum = deaths_per_million.reduce(
+    // 1.198.576 inhabitants in Balearic Islands
+    // Source INE, July 2019 data: https://www.ine.es/jaxiT3/Datos.htm?t=9681#!tabs-mapa
+    // to calculate deaths per million inhabitants we apply a 1/1.198576 factor
+    let deathsMillionPerDateInWindow = dataInWindow.map((item) => {
+      return (item.deaths_per_date/1.198576); });
+    let sum = deathsMillionPerDateInWindow.reduce(
       (previous, current) => (current += previous)
     );
-    let avg = (sum / deaths_per_million.length).toFixed(2);
+    let avg = (sum / deathsMillionPerDateInWindow.length).toFixed(2);
     resultingSeries.push({moving_avg: avg, data: lastDateInWindow});
   }
-  console.log(resultingSeries)
   return resultingSeries;
 };
 
